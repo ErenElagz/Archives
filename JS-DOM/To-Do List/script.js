235d17cc72063b1a662823958732e8e0be7d7160
ErenElagz <erenelagoz@gmail.com>
2023-07-13 23:55:57 +0300

    update

--- JS-DOM/To-Do List/script.js
+++ JS-DOM/To-Do List/script.js
@@ -4,6 +4,7 @@ const InputButton = document.getElementById('InputButton');
 const ListTable = document.getElementById('ListTable');
 const UpdateButton = document.getElementsByClassName('UpdateButton');
 const DeleteButton = document.getElementsByClassName('DeleteButton');
+const DoneButton = document.getElementsByClassName('DoneButton');
 let notes = [];
 
 /* Functions */
@@ -14,14 +15,15 @@ function DisplayNotes() {
 			'afterbegin',
 			`
                 <li class="list-group-item d-flex align-items-center justify-content-between">
-                    <div>
-                        ${i}
-                    </div>
+                    <p>${i}</p>
                     <div class="d-flex gap-2">
-                        <button class="btn btn-primary">
-                            <i class="bi bi-pen-fill UpdateButton" ></i>
+                        <button class="btn btn-primary" onclick="UpdateNote(this)" >
+                            <i class="bi bi-pen-fill UpdateButton"></i>
                         </button>
-                        <button class="btn btn-danger">
+                        <button class="btn btn-success" onclick="CompleteNote(this)" >
+                            <i class="bi bi-check2 DoneButton"></i>
+                        </button>
+                        <button class="btn btn-danger" onclick="DeleteNote(this)" >
                             <i class="bi bi-trash-fill DeleteButton" ></i>
                         </button>
                     </div>
@@ -42,11 +44,16 @@ function AddNote() {
 }
 
 /* Delete Note */
-function DeleteNote() {
-
+function DeleteNote(x) {
+	console.log(x);
 }
 
 /* Update Note */
-function UpdateNote() {
+function UpdateNote(x) {
+	console.log(x);
+}
 
+/* Complete Note */
+function CompleteNote(x) {
+	console.log(x);
 }
